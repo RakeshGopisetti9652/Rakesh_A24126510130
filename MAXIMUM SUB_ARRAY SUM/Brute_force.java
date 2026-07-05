d834f21bdb2ef6a7377a328c18d8af870181ee87
Rakesh <rakeshgopisetti9652@gmail.com>
2026-07-05 10:16:14 +0530

    Update maximum subarray Java files

--- MAXIMUM SUB_ARRAY SUM/Brute_force.java
+++ MAXIMUM SUB_ARRAY SUM/Brute_force.java
@@ -1,22 +1,35 @@
-public class Brute_force {
-    public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int bestSum = arr[0];
-        int start = 0, end = 0;
+import java.util.*;
+public class Brute_force{
+    public static int maxSubArraySum(int arr[]){
+        int currsum = 0;
+        int maxSum = Integer.MIN_VALUE; 
+        for(int i=0;i<arr.length;i++){
+            int start = i;
+            
 
-        for (int i = 0; i < arr.length; i++) {
-            int currentSum = 0;
-            for (int j = i; j < arr.length; j++) {
-                currentSum += arr[j];
-                if (currentSum > bestSum) {
-                    bestSum = currentSum;
-                    start = i;
-                    end = j;
+            for(int j=i;j<arr.length;j++){
+                int end = j;
+                currsum=0;
+                for(int k=start;k<=end;k++){
+                    currsum+=arr[k];
+                }
+                if(maxSum<currsum){
+                    maxSum=currsum;
                 }
             }
         }
-
-        System.out.println("Maximum subarray sum: " + bestSum);
-        System.out.println("Subarray from " + start + " to " + end);
+        return maxSum;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        System.out.println(maxSubArraySum(arr));
+        sc.close();
     }
 }
--- MAXIMUM SUB_ARRAY SUM/Kadanes.java
+++ MAXIMUM SUB_ARRAY SUM/Kadanes.java
@@ -1,14 +1,24 @@
+import java.util.*;
 public class Kadanes {
-    public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int currentSum = arr[0];
-        int bestSum = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            currentSum = Math.max(arr[i], currentSum + arr[i]);
-            bestSum = Math.max(bestSum, currentSum);
+    public static int maxSubArraySum(int arr[]){
+        int maxSum = Integer.MIN_VALUE;
+        int currsum = 0;
+        for(int i=0;i<arr.length;i++){
+            currsum = Math.max(arr[i], currsum+arr[i]);
+            maxSum = Math.max(maxSum, currsum);
         }
-
-        System.out.println("Maximum subarray sum: " + bestSum);
+        return maxSum;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        System.out.println(maxSubArraySum(arr));
+        sc.close();
     }
 }
